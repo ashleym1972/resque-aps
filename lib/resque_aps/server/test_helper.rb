fb1aa4106da6681887edab66dd07f9a4037c7bf8
Ashley Martens <ashleym_72@yahoo.com>
2010-07-07 16:58:09 -0700

    Add the test helper from the newer version of resque.

---
+++ lib/resque_aps/server/test_helper.rb
@@ -0,0 +1,20 @@
+require 'rack/test'
+require 'resque/server'
+
+module ResqueAps
+  module TestHelper
+    class Test::Unit::TestCase
+      include Rack::Test::Methods
+      def app
+        Resque::Server.new
+      end
+
+      def self.should_respond_with_success
+        test "should respond with success" do
+          assert last_response.ok?, last_response.errors
+        end
+      end
+    end
+  end
+end
+
